1499a82bd7556bbf2d929b4b67ed062a8367b865
Naureen <nmuk0004@student.monash.edu>
2020-11-23 13:15:09 +1100

    Final Changes to the application

--- Fit5140_ChatBot/View Controllers/HomeViewController.swift
+++ Fit5140_ChatBot/View Controllers/HomeViewController.swift
@@ -6,15 +6,21 @@
 //  Copyright © 2020 Monash University. All rights reserved.
 //  Reference: 1. https://www.youtube.com/watch?v=1HN7usMROt8&ab_channel=CodeWithChris
 
-// This is the home page of the application 
+// We are using the data from the Department of Human Health and Services of Victoria to keep the user updated with the current situation of the of the Coronavirus Pandemic. Reference : - https://www.dhhs.vic.gov.au/latest-news-and-data-coronavirus-covid-19
+
+// This is the home page of the application
 
 import UIKit
-
+ 
 class HomeViewController: UIViewController {
 
-
+    @IBOutlet weak var dhhsWebsiteLink: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        dhhsWebsiteLink.isEditable = false
+        dhhsWebsiteLink.dataDetectorTypes = .link
 
 
         // Do any additional setup after loading the view.
